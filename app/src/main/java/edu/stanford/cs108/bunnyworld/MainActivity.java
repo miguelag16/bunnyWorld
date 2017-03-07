a9baf323a856e93f295b0a6ea03b79e20ce25284
Th-eProgrammer <Dominicabbondanzo@gmail.com>
2017-03-06 23:32:23 -0800

    just tested git commit

--- app/src/main/java/edu/stanford/cs108/bunnyworld/MainActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/MainActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //just testing out github pushes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
