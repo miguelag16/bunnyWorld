6f43ea58beb312f4a5b0ee0e63519d662d21760d
Miguel A. Garcia <miguel16@gmail.com>
2017-03-09 22:27:22 -0800

    fleshed out shapecreator.xml/java

--- app/src/main/java/edu/stanford/cs108/bunnyworld/ShapeCreator.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/ShapeCreator.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 /**
  * Created by miguelgarcia on 3/8/17.
@@ -23,7 +26,7 @@ public class ShapeCreator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shape_creator);
 
-        Spinner spinner = (Spinner) findViewById(R.id.res_spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.image_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.shapes_array, android.R.layout.simple_spinner_item);
@@ -31,5 +34,20 @@ public class ShapeCreator extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        ArrayList<String> x = new ArrayList<String>();
+        x.add("miguel");
+        x.add("dom");
+        x.add("stan");
+        x.add("garcia");
+        x.add("abbondanzo");
+        x.add("chernavsky");
+        x.add("barcelona");
+
+        ListView list = (ListView) findViewById(R.id.script_list);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, x);
+
+        list.setAdapter(itemsAdapter);
     }
 }
