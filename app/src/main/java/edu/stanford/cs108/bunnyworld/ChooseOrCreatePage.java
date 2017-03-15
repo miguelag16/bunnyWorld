e0118c42ffc7957bc293b66add6be0448f99e0f3
Stan Chernavsky <stanmc@stanford.edu>
2017-03-14 22:51:31 -0700

    Prevented keyboard from popping up in ChooseOrCreatePage

--- app/src/main/java/edu/stanford/cs108/bunnyworld/ChooseOrCreatePage.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/ChooseOrCreatePage.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -44,6 +45,8 @@ public class ChooseOrCreatePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_or_create_page);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         TextView tv = (TextView) findViewById(R.id.bookName);
         tv.setText(cbs.getCurrentBook().getName());
