ab0130651f1ee88b439d3a6ae62ff513ada5fe74
Stan Chernavsky <stanmc@stanford.edu>
2017-03-17 16:25:24 -0700

    Added capability to read in from savedbooks.ser (from raw directory)

--- app/src/main/java/edu/stanford/cs108/bunnyworld/FileStorage.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/FileStorage.java
@@ -9,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
@@ -22,7 +23,7 @@ import static java.security.AccessController.getContext;
 
 public class FileStorage {
 
-    static String filename = "savedBooks.ser";
+    static String filename = "savedbooks.ser";
 
     public static void store(Context context, LinkedHashMap<Integer, Book> booksMap) throws IOException {
 
@@ -46,16 +47,14 @@ public class FileStorage {
 
     public static LinkedHashMap<Integer, Book> load(Context context) throws IOException, ClassNotFoundException {
 
-        File path = context.getFilesDir();
-        String file = path.toString() + "/" + filename;
-
-        LinkedHashMap<Integer, Book> loadedBooksMap = null;
+        // Get a books map that contains the default world from the raw directory
+        InputStream rawStream = context.getResources().openRawResource(R.raw.savedbooks);
+        LinkedHashMap<Integer, Book> defaultBooksMap = null;
         FileInputStream fis = null;
         ObjectInputStream in = null;
         try {
-            fis = new FileInputStream(file);
-            in = new ObjectInputStream(fis);
-            loadedBooksMap = (LinkedHashMap<Integer, Book>)in.readObject();
+            in = new ObjectInputStream(rawStream);
+            defaultBooksMap = (LinkedHashMap<Integer, Book>)in.readObject();
             in.close();
         }
         catch(IOException ex) {
@@ -65,7 +64,32 @@ public class FileStorage {
             ex.printStackTrace();
         }
 
-        return loadedBooksMap;
+
+        // Get all the other worlds stored locally on the emulator
+        File path = context.getFilesDir();
+        String file = path.toString() + "/" + filename;
+        LinkedHashMap<Integer, Book> loadedBooksMap = null;
+        FileInputStream fis2 = null;
+        ObjectInputStream in2 = null;
+        try {
+            fis2 = new FileInputStream(file);
+            in2 = new ObjectInputStream(fis2);
+            loadedBooksMap = (LinkedHashMap<Integer, Book>)in2.readObject();
+            in2.close();
+        }
+        catch(IOException ex) {
+            ex.printStackTrace();
+        }
+        catch(ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+
+
+        // Combine default world stored in raw directory with other worlds stored on emulator
+        LinkedHashMap<Integer, Book> completeBooksMap = new LinkedHashMap<Integer, Book>();
+        completeBooksMap.putAll(defaultBooksMap);
+        completeBooksMap.putAll(loadedBooksMap);
+        return completeBooksMap;
     }
 
 }
